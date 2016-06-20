9f63adc4897d9814f85a961efc6ee9df040212ed
lubna alqadah <lubna alqadah>
2016-06-19 18:28:05 -0700

    Assignment 1

---
+++ script.js
@@ -0,0 +1,177 @@
+////Step 1
+//
+//var someMonth;
+//function theMonth() ;
+//const currentMonth;
+//var summerMonth = new Array;
+//function myLibraryAFunction();
+//
+//
+////Step 2
+//
+//"Hellow" //string literal
+//7 //numeric literal
+//false //boolean literal
+//null // null literal
+//
+//
+////Step 3
+//
+//var x = 5, y = 7;
+//var sum = x + y;
+//var age = x * y;
+
+//Step 4
+
+var strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, blnMayWeContactYou;
+
+////Step 5
+//
+//strFirstName = "lubna";
+//strLastName = window.prompt("Please enter your last name.");
+//strAddress = window.prompt("Please enter your address.");
+//strCity = "San Diego";
+//strState = "CA";
+//intZipCode = window.prompt("please enter your zip code.");
+//intYourAge = window.prompt("Please enter your age.");
+//strReferralSource = window.prompt("Please enter your age.");
+//blnMayWeContactYou = window.confirm("May we contact you?");
+//
+////Step 6
+//
+//var first = 77 + " Hellow";
+//console.log(first);
+//
+//var second = false + " String";
+//console.log(second);
+//
+//var third = 232 + true;
+//console.log(third);
+
+
+//step 7
+
+//var firstName = "Lubna";
+//console.log(firstName + " " + lastName);
+//lastName = "Qadah";
+
+
+// Step 8
+
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+//
+
+//Step 9
+
+//var x;
+//var y = null;
+//console.log(x);
+//console.log(y);
+
+//step 10
+
+//var x = "Hellow";
+//console.log(typeof(x));
+//
+//var y = 7;
+//console.log(typeof(y));
+//
+//var z = false;
+//console.log(typeof(z));
+//
+//var v = [1, 2, 3];
+//console.log(typeof(v));
+//
+//var w;
+//console.log(typeof(w));
+
+//Step 11
+
+//alert("Hello Zak Ruvalcaba," + " welcome to the JavaScript class!");
+//
+////Step 12
+
+var name = "Lubna Qadah";
+
+//alert("Hello " + name + " welcome to the JavaScript class!");
+
+
+//Step 13
+
+var course = "JavaScript";
+//alert("Hello " + name + " welcome to the " + course + " class!");
+
+
+//Step 14
+
+//alert("Hello " + name+ ". \nwelcome to the " + course + " class!");
+
+
+//Step 15
+
+//name = window.prompt("Please enter your name.");
+//alert("Hello " + name+ ". \nwelcome to the " + course + " class!");
+
+//Step 16
+
+//course = window.prompt("Please enter the class you are taking.");
+//alert("Hello " + name+ ". \nwelcome to the " + course + " class!");
+
+// Step 17
+
+//var x;
+//x = 10;
+//
+//var y;
+//y = 20;
+//
+//console.log(x + y);
+//
+////Step 18
+//
+//var x = 20;
+//x += 20;
+//console.log(x);
+
+//Step 19
+
+//var x = 20;
+//x *= 5;
+//console.log(x);
+
+//Step 20
+
+//var x = 20 % 3;
+//x /= 1;
+//console.log(x);
+
+//Step 21
+
+//console.log(23 < 4 || 3 < 5);
+
+//Step 22
+
+//console.log(23 < 4 && 3 < 5);
+
+//Step 23
+
+//var widget = new Object;
+//console.log(typeof(widget));
+//
+////Step 24
+//
+//widget = [1, 2, 3, 4];
+//console.log(widget instanceof Object);
+//
+//
+////Step 25
+//
+//widget = [1, 2, 3, 4];
+//console.log(widget instanceof String);
+
+
+
+
+
+
